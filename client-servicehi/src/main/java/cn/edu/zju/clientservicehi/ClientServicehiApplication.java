d0dbbdcf19555634b80aee2c3dfc791380a81965
Tony Yu <602232939@qq.com>
2018-03-25 17:05:12 +0800

    finish eureka server&client

---
+++ client-servicehi/src/main/java/cn/edu/zju/clientservicehi/ClientServicehiApplication.java
@@ -0,0 +1,26 @@
+package cn.edu.zju.clientservicehi;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableEurekaClient
+@RestController
+public class ClientServicehiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientServicehiApplication.class, args);
+	}
+
+    @Value("${server.port}")
+    String port;
+    @RequestMapping("/hi")
+    public String home(@RequestParam String name) {
+        return "hi "+name+",i am from port:" +port;
+    }
+}
