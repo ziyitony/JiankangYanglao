83efd4473bd23f16d46ca6ed42dc730032738295
Tony Yu <602232939@qq.com>
2018-03-25 21:25:06 +0800

    finish RestTemplate+Ribbon

---
+++ service-ribbon/src/main/java/cn/edu/zju/serviceribbon/Controller.java
@@ -0,0 +1,17 @@
+package cn.edu.zju.serviceribbon;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+    @Autowired
+    HelloService helloService;
+    
+    @RequestMapping(value = "/hi")
+    public String hi(@RequestParam String name) {
+        return helloService.hiService(name);
+    }
+}
