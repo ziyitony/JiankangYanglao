e2edefc35bc1e0c226b1a2416da489b2a8d413df
Tony Yu <602232939@qq.com>
2018-03-26 12:28:30 +0800

    1. switch spring-cloud from Finchley M9 to Edgware SR2 & spring-boot from 2.0.0.RELEASE to 1.5.10.RELEASE, cause dependencies for latest version are not stable. 2. uncomment feign module cause now it is working. 3. finish hystrix with ribbon and feign.

---
+++ service-feign/src/main/java/cn/edu/zju/servicefeign/SchedualServiceHiHystric.java
@@ -0,0 +1,11 @@
+package cn.edu.zju.servicefeign;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class SchedualServiceHiHystric implements SchedualServiceHi {
+    @Override
+    public String sayHiFromClientOne(String name) {
+        return "sorry "+name;
+    }
+}
